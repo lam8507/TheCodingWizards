79a3be0f43b504b4b698eb647524eb60342a731f
michelleliu83 <79758219+michelleliu83@users.noreply.github.com>
2021-03-10 12:30:30 -0500

    Update generalUSpopvisual.m

--- generalUSpopvisual.m
+++ generalUSpopvisual.m
@@ -1,7 +1,12 @@
-% this portion was done by Sarah
-X = [5.9 60.1 18.5 13.4 2.8 1.3]; % generic US population pie chart
-% data from census
-% census data was only hard coding
+% Using Census data to create a pie chart of the United States population racial profile
+% Data accessed at: https://www.census.gov/quickfacts/fact/table/US/IPE120219
+
+X = [5.9 60.1 18.5 13.4 2.8 1.3];
+
+% Census data analysis was the only case of hard coding
 labels = {'Asian', 'White', 'Hisp', 'Black', 'Other', 'Native'};
+% Creating the pie chart
 pie(X, labels)
 title('Population of the US')
+
+% Coding and debugging by Sarah, 1 hour
