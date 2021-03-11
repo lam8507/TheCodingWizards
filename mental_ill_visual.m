6a2678ed5b54645d7981b5c44ca8e3afc21d8229
laurelmyers <laurelmyers@laurels-mbp.lan>
2021-03-10 22:01:09 -0500

    GUI created by Laurel--12 hours total

--- mental_ill_visual.m
+++ mental_ill_visual.m
@@ -1,6 +1,7 @@
 % simple portion done by Sarah
 T = readtable('fatal-police-shootings-data.csv');
 mental_illness = T(:,11);
+race = T(:, 8);
 % pie chart for history of mental illness
 mental_illness_cell = table2cell(mental_illness);
 tbl_mental_illness = tabulate(mental_illness_cell);
